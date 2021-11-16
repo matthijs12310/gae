62b739e37742ee24c7475a4130a8379d263bf437
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2021-11-16 12:08:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,2 @@
 import os
-import requests
-url = 'https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v1.62.2/openvscode-server-v1.62.2-linux-x64.tar.gz'
-r = requests.get(url, allow_redirects=True)
-open('openvscode-server-v1.62.2-linux-x64.tar.gz', 'wb').write(r.content)
 os.system("tar -xzf openvscode-server-v1.62.2-linux-x64.tar.gz && cd openvscode-server-v1.62.2-linux-x64 && ./server.sh")
